8c7c4faf435001dfc2b06a6c11b3d80715adfdc3
alexis-opolka <53085471+alexis-opolka@users.noreply.github.com>
2024-03-01 13:46:42 +0100

    Start add wlanguage support

---
+++ components/prism-wlanguage.js
@@ -0,0 +1,33 @@
+Prism.languages['wlanguage'] = {
+    "string": {
+        pattern: /["\[](.*?)["\]]/,
+        greedy: false
+    },
+    "comment": {
+        pattern: /\/\/.*/,
+        greedy: true
+    },
+    "function": {
+        pattern: /([a-zA-Z_]\w*)\s*(?=\s*\()/g,
+    },
+    "function-definition": {
+        pattern: /((?:^|\s)procédure[ \t]+).*?([a-zA-Z_]\w*)(?=\s*\()/g,
+        lookbehind: true,
+        alias: "function"
+    },
+    "property": {
+        pattern: /(?<=[.])(\w+)/g,
+        lookforward: true,
+        greedy: true
+    },
+    "": / /,
+    // TODO: Modify the `number` RegEx to match the WLanguage syntax
+    "number": /\b0(?:b(?:_?[01])+|o(?:_?[0-7])+|x(?:_?[a-f0-9])+)\b|(?:\b\d+(?:_\d+)*(?:\.(?:\d+(?:_\d+)*)?)?|\B\.\d+(?:_\d+)*)(?:e[+-]?\d+(?:_\d+)*)?j?(?!\w)/i,
+    "operator": /[+-]{1,2}|[-%=]=?|!=|:=|\*\*?=?|<[<=>]?|>[=>]?|[&|^~]|Modulo|\//,
+    "boolean": /\b(?:Vrai|Faux|Null)\b/,
+    "punctuation": /[{}[\];(),.:]/,
+    "keyword": /\b(?:_(?=\s*:)|procédure|globale|locale|est|un|une)\b/,
+    "builtin": /\b(?:booléen|entier|chaîne|Email|emailSessionSMTP|emailOptionDéfaut)\b/,
+}
+
+Prism.languages.webmanifest = Prism.languages.json
